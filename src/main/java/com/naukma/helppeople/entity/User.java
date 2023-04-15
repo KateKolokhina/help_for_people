9d28eb7d9eb1f3575168c9930154a5aa9899409e
BlackKyiv <62792985+BlackKyiv@users.noreply.github.com>
2023-04-15 20:34:11 +0200

    Added some stuff

--- src/main/java/com/naukma/helppeople/entity/User.java
+++ src/main/java/com/naukma/helppeople/entity/User.java
@@ -31,7 +31,7 @@ public class User implements Serializable {
     @Column(nullable = false)
     private String password;
 
-    @Column(nullable = false)
+    @Column()
     private String phone;
 
     @Column(nullable = false)
